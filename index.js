14b81386322085f991563dff9d8a8021c94720c7
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-08-13 10:01:05 -0300

    init commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+app.get("/:usuario/:repositorio", (req, res) => {
+  console.log(req.params.usuario, req.params.repositorio);
+  console.log("depois do :?", req.query.id, req.query.idade);
+  console.log("corpo da requisição:", req.body.cidade, req.body.bairro);
+  res.send("Tá frio demais");
+});
+
+app.listen(port, () => {
+  console.log("Conexão estabelecida");
+});
